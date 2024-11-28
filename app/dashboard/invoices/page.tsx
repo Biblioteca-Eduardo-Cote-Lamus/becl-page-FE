4c65a77336990aa5b81cc55fd8a5b701f448a65a
Anderson Patiño <andersondavidpc@ufps.edu.co>
2024-11-28 15:42:43 -0500

    Definir la interfaz para las props de la página de edición de facturas

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -13,14 +13,14 @@ export const metadata: Metadata = {
   title: 'Invoices',
 };
 
-export default async function Page({
-  searchParams,
-}: {
+interface PageProps {
   searchParams?: {
     query?: string;
     page?: string;
   };
-}) {
+}
+
+export default async function Page({ searchParams }: PageProps) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
 
