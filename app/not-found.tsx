ff90e3a3fc5bd3a79ab60e3375e55c3a66b3f318
Anderson PC Marcos <andersondavidpc@ufps.edu.co>
2025-03-17 12:22:47 -0500

    Refactor: Enhance code formatting in not-found component for improved readability

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,18 +4,18 @@ const Custom404 = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white shadow-md rounded-md p-4">
-      <h1 className="text-4xl font-bold text-red-600">
-        404 - Página no encontrada
-      </h1>
-      <p className="my-4 text-lg text-gray-700">
-        Lo sentimos, la página que buscas no existe.
-      </p>
-      <a
-        href="/"
-        className="mt-10 px-4 py-2 bg-secondaries_red-900 text-white rounded hover:bg-red"
-      >
-        Volver al inicio
-      </a>
+        <h1 className="text-4xl font-bold text-red-600">
+          404 - Página no encontrada
+        </h1>
+        <p className="my-4 text-lg text-gray-700">
+          Lo sentimos, la página que buscas no existe.
+        </p>
+        <a
+          href="/"
+          className="mt-10 px-4 py-2 bg-secondaries_red-900 text-white rounded hover:bg-red"
+        >
+          Volver al inicio
+        </a>
       </div>
     </div>
   );
