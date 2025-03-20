a2ac0df7fbed722fbdcbcd78a98e0cb036705e59
Anderson PC Marcos <andersondavidpc@ufps.edu.co>
2025-03-20 12:29:11 -0500

    Update pnpm-lock.yaml to replace bcrypt with bcryptjs, adjust autoprefixer and tailwindcss versions, and refactor postcss.config.js for improved plugin syntax. Remove tailwind.config.js as part of the cleanup process. Update layout metadata for Biblioteca UFPS to reflect new branding and content.

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,11 +4,31 @@ import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: {
-    template: "Biblioteca Eduardo Cote Lamus - %s",
-    default: "Biblioteca Eduardo Cote Lamus",
+    template: "Biblioteca UFPS - %s",
+    default: "Biblioteca UFPS",
   },
-  description: "The official Next.js Learn Dashboard built with App Router.",
-  metadataBase: new URL("https://next-learn-dashboard.vercel.sh"),
+  description:
+    "Portal de la División de Biblioteca de la Universidad Francisco de Paula Santander - Cúcuta, Norte de Santander",
+  metadataBase: new URL("https://biblioteca.ufps.edu.co"),
+  applicationName: "Biblioteca UFPS",
+  authors: [
+    { name: "División de Biblioteca UFPS" },
+    { name: "Biblioteca Eduardo Cote Lamus" },
+    { name: "Programa de Ingeniería de Sistemas UFPS" },
+    { name: "Marcos David Mejía Gómez" },
+    { name: "Anderson David Patiño Caicedo" },
+  ],
+  keywords:
+    "ufps, biblioteca, universidad, francisco de paula santander, cucuta, colombia, norte de santander, préstamos, catálogo, recursos bibliográficos, servicios bibliotecarios, paz y salvo, biblioteca eduardo cote lamus, bases de datos",
+  openGraph: {
+    title: "Biblioteca UFPS",
+    type: "website",
+    url: "https://biblioteca.ufps.edu.co",
+    description:
+      "Portal de la División de Biblioteca de la Universidad Francisco de Paula Santander - Cúcuta, Norte de Santander",
+    siteName: "Biblioteca UFPS",
+  },
+  viewport: "width=device-width, initial-scale=1.0",
 };
 
 export default function RootLayout({
