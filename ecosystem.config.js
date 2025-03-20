a2790033b9acbd3206836efd83bbe014fe949fc0
Biblioteca_UFPS <biblioteca@ufps.edu.co>
2025-03-20 14:16:34 -0500

    Update ecosystem.config.js for PM2 deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,1 +1,10 @@
- 
+module.exports = {
+  apps: [{
+    name: 'nextjs-dashboard',
+    script: '.next/standalone/server.js',
+    env: {
+      PORT: 3000,
+      NODE_ENV: 'production'
+    }
+  }]
+}
