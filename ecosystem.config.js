27933bc0b1d97b19433592a3cb329a4bcf9806ea
Anderson PC Marcos <andersondavidpc@ufps.edu.co>
2025-03-20 13:38:51 -0500

    Remove tailwind.config.js as part of the project cleanup process.

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [{
+    name: 'nextjs-dashboard',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+} 
