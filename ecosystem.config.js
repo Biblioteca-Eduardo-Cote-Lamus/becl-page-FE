165696a10c04b381d8f4bf23498667660d18604e
Biblioteca_UFPS <biblioteca@ufps.edu.co>
2025-03-20 15:14:47 -0500

    Update ecosystem.config.js for production deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   apps: [{
     name: 'nextjs-dashboard',
-    script: '.next/standalone/server.js',
+    script: 'node_modules/next/dist/bin/next',
+    args: 'start',
     env: {
       PORT: 3000,
       NODE_ENV: 'production'
