f9a2e41d1507622494782d02ea7f3607e2a9a0f3
Anderson PC Marcos <andersondavidpc@ufps.edu.co>
2025-03-20 13:50:08 -0500

    Remove ecosystem.config.js as part of project cleanup.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,11 +1,1 @@
-module.exports = {
-  apps: [{
-    name: 'nextjs-dashboard',
-    script: 'node_modules/next/dist/bin/next',
-    args: 'start',
-    env: {
-      NODE_ENV: 'production',
-      PORT: 3000
-    }
-  }]
-} 
+ 
